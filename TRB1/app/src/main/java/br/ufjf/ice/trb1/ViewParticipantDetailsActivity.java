ee2b5d11f4880cca45b309267a89c748b13ab040
J. Gabriel Maia <jonagabriel@gmail.com>
2018-10-06 13:49:42 -0300

    Creating Participant Details View
    Creating activity to view the details of a participant

---
+++ TRB1/app/src/main/java/br/ufjf/ice/trb1/ViewParticipantDetailsActivity.java
@@ -0,0 +1,13 @@
+package br.ufjf.ice.trb1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewParticipantDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_participant_details);
+    }
+}
