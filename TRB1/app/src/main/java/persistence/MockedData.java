7431784ed07bf08129ef500a9856e8fcc8a4410c
J. Gabriel Maia <jonagabriel@gmail.com>
2018-10-06 14:27:05 -0300

    Criando dados ficticios
    Simulando persistência através de dados ficticios.

---
+++ TRB1/app/src/main/java/persistence/MockedData.java
@@ -0,0 +1,12 @@
+package persistence;
+
+import java.util.ArrayList;
+
+import model.Event;
+import model.Participant;
+
+public class MockedData {
+    ArrayList<Event> eventList = new ArrayList<Event>();
+    ArrayList<Participant> participantList = new ArrayList<Participant>();
+    
+}
