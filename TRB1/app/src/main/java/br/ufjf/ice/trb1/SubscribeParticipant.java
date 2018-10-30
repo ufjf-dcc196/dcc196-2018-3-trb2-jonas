686ee88dc923148749685fcd820db0f80b26e782
J. Gabriel Maia <jonagabriel@gmail.com>
2018-10-29 23:56:33 -0300

    Adicionando Acitivy para inscrever participantes em evento

--- TRB1/app/src/main/java/br/ufjf/ice/trb1/SubscribeParticipant.java
+++ TRB1/app/src/main/java/br/ufjf/ice/trb1/SubscribeParticipant.java
@@ -3,11 +3,11 @@ package br.ufjf.ice.trb1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CreateEventActivity extends AppCompatActivity {
+public class SubscribeParticipant extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_event);
+        setContentView(R.layout.activity_subscribe_participant);
     }
 }
