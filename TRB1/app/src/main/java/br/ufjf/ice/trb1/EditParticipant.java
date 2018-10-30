0a09ceb408063cccd9e1f951e9ad91248869cc77
J. Gabriel Maia <jonagabriel@gmail.com>
2018-10-29 23:27:15 -0300

    Adicionando activity de edição de dados de participante

---
+++ TRB1/app/src/main/java/br/ufjf/ice/trb1/EditParticipant.java
@@ -0,0 +1,13 @@
+package br.ufjf.ice.trb1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditParticipant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_participant);
+    }
+}
