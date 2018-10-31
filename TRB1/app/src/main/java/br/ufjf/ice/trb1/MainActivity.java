4b78af89dd7e1fe0f4ff4246cd011a874fbf3d78
J. Gabriel Maia <jonagabriel@gmail.com>
2018-10-30 22:13:29 -0300

    Adicionando navegacao de rodape; trocando activities por fragments e eliminando mudancas para recyclerviews

--- TRB1/app/src/main/java/br/ufjf/ice/trb1/MainActivity.java
+++ TRB1/app/src/main/java/br/ufjf/ice/trb1/MainActivity.java
@@ -1,7 +1,13 @@
 package br.ufjf.ice.trb1;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView nav = findViewById(R.id.bottomNavigationView);
+
+        nav.setOnNavigationItemSelectedListener(navigationListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainFragment,  new GetAllParticipantsFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationListener =
+    new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()){
+                case R.id.navigation_participants :
+                    selectedFragment = new GetAllParticipantsFragment();
+                    break;
+
+                case R.id.navigation_events :
+                    selectedFragment = new GetAllEventsFragment();
+                    break;
+                case R.id.navigation_register:
+                    selectedFragment = new RegisterItemFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.mainFragment, selectedFragment).commit();
+            return true;
+        }
+    };
 }
