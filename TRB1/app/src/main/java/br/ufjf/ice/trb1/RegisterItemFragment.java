4b3cedd8245520cc73ee6e2e03242eb8372af374
J. Gabriel Maia <jonagabriel@gmail.com>
2018-10-30 22:36:45 -0300

    Trocando visualizacao de botoes de selecao para fragments de cadastro de evento ou participante

--- TRB1/app/src/main/java/br/ufjf/ice/trb1/RegisterItemFragment.java
+++ TRB1/app/src/main/java/br/ufjf/ice/trb1/RegisterItemFragment.java
@@ -3,15 +3,76 @@ package br.ufjf.ice.trb1;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegisterItemFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_register_item, container, false);
+        View view =  inflater.inflate(R.layout.fragment_register_item, container, false);
+
+        ViewPager viewPager = view.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabs = view.findViewById(R.id.result_tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        return view;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+
+        Adapter adapter = new Adapter(getChildFragmentManager());
+        adapter.addFragment(new RegisterParticipantFragment(), getResources().getString(R.string.register_participant));
+        adapter.addFragment(new RegisterEventFragment(), getResources().getString(R.string.register_event));
+
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
