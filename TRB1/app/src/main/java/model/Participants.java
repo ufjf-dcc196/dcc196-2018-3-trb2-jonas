1c3640146c772160953586620d37fead4b0bd24d
J. Gabriel Maia <jonagabriel@gmail.com>
2018-11-10 20:53:04 -0200

    Hotfix: removendo WIP de edição de evento

--- TRB1/app/src/main/java/model/Participants.java
+++ TRB1/app/src/main/java/model/Participants.java
@@ -11,7 +11,7 @@ public class Participants {
     private Participants(){
         setAllParticipants(new ArrayList<Participant>());
         allParticipants.add(new Participant("Harry Potter", "hp@hogwarts.castle","123456780"));
-        allParticipants.add( new Participant("Hermione Granger", "hermione.granger@hogwarts.castle","123456781"));
+        allParticipants.add(new Participant("Hermione Granger", "hermione.granger@hogwarts.castle","123456781"));
         allParticipants.add(new Participant("Albus Dumbledore", "theboss@hogwarts.castle","123456782"));
         allParticipants.add(new Participant("Draco Malfoy", "malfoydrake@hogwarts.castle","123456783"));
         allParticipants.add(new Participant("Ronald Weasley",  "ron.weasley@hogwarts.castle","123456784"));
